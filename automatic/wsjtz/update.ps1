c44dcb4178844128b580f51125d636ea97d54b57
chtof <chtof62@gmail.com>
2026-07-10 23:39:05 +0200

    Create update.ps1

---
+++ automatic/wsjtz/update.ps1
@@ -0,0 +1,33 @@
+﻿import-module au
+. ..\..\helpers\GitHub_Helper.ps1
+
+function global:au_BeforeUpdate { Get-RemoteFiles -NoSuffix -Purge }
+
+function global:au_GetLatest {
+   return github_GetInfo -ArgumentList @{
+        repository = 'sq9fve/wsjt-z'
+        regex32    = 'wsjtz-[\d\.]+-(?<Version>[\d\.]+)-win32.exe$'
+        regex64    = 'wsjtz-[\d\.]+-(?<Version>[\d\.]+)-win64.exe$'
+   }
+}
+
+function global:au_SearchReplace {
+    @{
+       "legal\VERIFICATION.txt"  = @{
+            "(?i)(x32: ).*"             = "`${1}$($Latest.URL32)"
+            "(?i)(x64: ).*"             = "`${1}$($Latest.URL64)"
+            "(?i)(checksum type:\s+).*" = "`${1}$($Latest.ChecksumType32)"
+            "(?i)(checksum32:).*"       = "`${1} $($Latest.Checksum32)"
+            "(?i)(checksum64:).*"       = "`${1} $($Latest.Checksum64)"
+        }
+
+        "tools\chocolateyinstall.ps1" = @{
+          "(?i)(^\s*file\s*=\s*`"[$]toolsDir\\)(.*)`""   = "`$1$($Latest.FileName32)`""
+          "(?i)(^\s*file64\s*=\s*`"[$]toolsDir\\)(.*)`""   = "`$1$($Latest.FileName64)`""
+        }
+    }
+}
+
+if ($MyInvocation.InvocationName -ne '.') { # run the update only if script is not sourced
+    update -ChecksumFor none
+}
