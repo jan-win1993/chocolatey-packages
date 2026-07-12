0748f573e6940f2a6efe6e20a0108b9ef9331588
Chocolatey <chocolatey@realdimensions.net>
2026-07-12 16:27:10 +0000

    AU: 2 updated - photoflare photoflare.install [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/1c982bc1d7f197018e066f39d36c9950955c72fb

--- automatic/photoflare.install/tools/chocolateyinstall.ps1
+++ automatic/photoflare.install/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName = $env:ChocolateyPackageName
   fileType    = 'EXE'  # ou 'MSI' ?
-  file64      = "$toolsDir\Photoflare_CE_1.6.5_amd64.msi"
+  file64      = "$toolsDir\Photoflare_CE_v1.7.2_amd64.exe"
   silentArgs  = "/qn /norestart /l*v `"$env:TEMP\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
 }
 
