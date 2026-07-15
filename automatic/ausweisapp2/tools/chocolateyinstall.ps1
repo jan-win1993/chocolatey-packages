60e6a603d56a96ca84b5a7f287e20e57af081aba
Chocolatey <chocolatey@realdimensions.net>
2026-07-15 16:30:23 +0000

    AU: 3 updated - ausweisapp2 cudatext gitextensions.portable [skip ci] https://gist.github.com/chtof/393704c64c2344f0f8e029cd57bebd2b/b443ec43e08c15fb89426b80174c83bd2a27bc29

--- automatic/ausweisapp2/tools/chocolateyinstall.ps1
+++ automatic/ausweisapp2/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName = $env:ChocolateyPackageName
   filetype    = 'MSI'
-  file        = "$toolsDir\AusweisApp-2.5.3.msi"
+  file        = "$toolsDir\AusweisApp-2.5.4.msi"
   silentArgs  = "/qn /norestart /l*v `"$env:TEMP\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
 }
 
--- automatic/gitextensions.portable/tools/chocolateyinstall.ps1
+++ automatic/gitextensions.portable/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   destination   = "$toolsDir"
-  file64        = "$toolsDir\GitExtensions-Portable-x64-7.0.1.86-c119a52.zip"
+  file64        = "$toolsDir\GitExtensions-Portable-x64-7.2.0.92-501f831.zip"
 }
 
 # Prevent chocolatey from creating shims for supplementary executables
